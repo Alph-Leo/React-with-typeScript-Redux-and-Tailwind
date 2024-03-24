33b2126929aba91ccd569917014f841a6af20d05
Saheed Dammy <saidex1@ymail.com>
2024-03-24 22:49:07 +0100

    update

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,44 +1,60 @@
 import users from '../assets/images/png/users.png'
 import book from '../assets/images/png/book-open.png'
-import briefcase from '../assets/images/png/briefcase.png'
 import user from '../assets/images/png/user.png'
 import groups from '../assets/images/png/Group 598.png'
+import {ReactComponent as BriefcaseIcon} from '../assets/images/svg/briefcase.svg'
+import React, { useState } from 'react'
 
-const MainBody = () => {
+const MainBody: React.FC = () => {
+
+    const [selectedItem, setSelectedItem] = useState<string | null>(null);
+    
+    const HandleClick = (item: string) =>{
+        setSelectedItem(item)
+    }
     return(
         <div className=' mt-48 flex space-x-32 '>
             <ul>
                 <div className=' ml-20'>
                     <li>
-                        <a href="https://" className=' flex space-x-4 hover:text-blue-500'><img src={users} alt="" />
-                        <h4 className=' font-semibold'>Cohorts</h4></a>
+                        <a href="#" className={`flex space-x-4 ${selectedItem ==='cohorts' ? 'text-blue-500' : ''}`} onClick={() => HandleClick('cohorts')}>
+                            <img src={user} alt="" className=' absolute'/>
+                            <img src={user} alt="" className=''/>
+                            <h4 className=' font-semibold'>Cohorts</h4>
+                        </a>
                     </li>
                 </div>
                 <div className=' ml-20 mt-10 '>
                     <li>
-                        <a href="https://" className='flex space-x-4 hover:text-blue-500 transition-colors font-semibold'><img src={book} alt="" />
-                        <h4 className=''>Programs</h4></a>
+                        <a href="#" className={`flex space-x-4 ${selectedItem ==='programs' ? 'text-blue-500' : ''}`} onClick={() => HandleClick('programs')}>
+                            <img src={book} alt="" />
+                            <h4 className=' font-semibold'>Programs</h4>
+                        </a>
                     </li>
                 </div>
                 <div className=' ml-20 mt-10'>
                     <li>
-                        <a href="https://" className='flex space-x-4 hover:text-blue-500 transition-colors font-semibold'><img src={briefcase} alt="" />
-                        <h4>Instructors</h4></a>
+                        <a href="#" className={`flex space-x-4 ${selectedItem ==='instructors' ? 'text-blue-500' : ''}`} onClick={() => HandleClick('instructors')}>
+                            <BriefcaseIcon className=' hover:to-blue-500'/>
+                            <h4 className=' font-semibold'>Instructors</h4>
+                        </a>
                     </li>
                 </div>
                 <div className=' ml-20 mt-10'>
                     <li>
-                        <a href="https://" className='flex space-x-4 hover:text-blue-500 transition-colors font-semibold'><img src={user} alt="" />
-                        <h4>Learners</h4></a>
+                        <a href="#" className={`flex space-x-4 ${selectedItem ==='learners' ? 'text-blue-500' : ''}`} onClick={() => HandleClick('learners')}>
+                            <img src={user} alt="" />
+                            <h4 className=' font-semibold'>Learners</h4>
+                        </a>
                     </li>
                 </div>
             </ul>
             <div className=' font-extrabold'><h1>Cohorts</h1></div>
             <div className=' mt-44'>
                 <img src={groups} alt="" />
-                <div className=' font-bold ml-10'><h2>Empty Space</h2></div>
-                <div>
-                    <p className=' w-[400px]'>No cohort has been created yet. let's get you started by clicking the button below</p>
+                <div className=' font-bold ml-5'><h2>Empty Space</h2></div>
+                <div className='h-20 w-[200px] px-7 py-5 rounded-2xl'>
+                    <a href="#"><button className='bg-blue-500 h-[40px] w-[150px]  rounded '><p className=' font-semibold text-white'>Create a Cohort</p></button></a>
                 </div>
             </div>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,17 @@ import ellipse from '../assets/images/png/Ellipse 131.png'
 import vector from '../assets/images/png/Vector.png'
 import unsplash from '../assets/images/png/unsplash_LWfFfA5U5z8.png'
 import frame1 from '../assets/images/png/Frame 12712.png'
-import frame2 from '../assets/images/png/Frame 12717.png'
+import  DropArrow from '../assets/images/svg/Vector.svg'
+import ArrowLeft from '../assets/images/png/arrow-left.png'
+import React, { useState } from 'react'
 
-const Header = () => {
+
+const Header: React.FC = () => {
+    const [selectedItem, setSelectedItems] = useState<string | null>(null);
+
+    const HandleClick = (item: string) => {
+        setSelectedItems(item)
+    }
     return(
         <header className=" h-[80px] border border-gray-300 shadow-md">
             <div className=" h-10 flex justify-center">
@@ -14,25 +22,32 @@ const Header = () => {
                     <a href="#"><img src={Images} alt="" className="w-[70px] h-6"/></a>
                 </div>
                 <nav className="w-[400px] h-24 justify-center space-x-8 ml-40 mt-6  ">
-                        <a href="https://" className=" font-medium rounded-full hover:text-blue-500 hover:bg-blue-100/50 hover:underline hover:underline-offset-[35px] ">Home</a>
-                        <a href="https://" className=" font-medium rounded-full hover:text-blue-500 hover:bg-blue-100/50 hover:underline hover:underline-offset-[35px] ">Workspace</a>
-                        <a href="https://" className=" font-medium rounded-full hover:text-blue-500 hover:bg-blue-100/50 hover:underline hover:underline-offset-[35px]">Resources Library</a>
+                        <a href="#" className={`font-medium ${selectedItem === 'home' ? 'text-blue-600 rounded-full bg-blue-100/50 underline underline-offset-[35px]' : ''}`} onClick={() => HandleClick('home')}>Home</a>
+                        <a href="#" className={`font-medium ${selectedItem === 'workspace' ? 'text-blue-600 rounded-full bg-blue-100/50 underline underline-offset-[35px]' : ''}`} onClick={() => HandleClick('workspace')}>Workspace</a>
+                        <a href="#" className={`font-medium ${selectedItem === 'resources library' ? 'text-blue-600 rounded-full bg-blue-100/50 underline underline-offset-[35px]' : ''}`} onClick={() => HandleClick('resources library')}>Resources Library</a>
                 </nav>
                 <nav className="flex w-[400px] h-24 justify-center space-x-8 mt-5 ml-10 ">
-                        <a href="https://" className=' h-6 mt-3'><img src={vector} alt="" /></a>
-                        <a href="https://" className=' h-4 mt-1'><img src={ellipse} alt="" /></a>
-                        <a href="https://" className=' flex font-medium mt-2'>Username
-                        <img src={vector} className=' h-4 mt-1 ml-2' alt="" />
+                        <a href="#" className=' h-6 mt-3'><img src={vector} alt="" /></a>
+                        <a href="#" className=' h-4 mt-1'><img src={ellipse} alt="" /></a>
+                        <a href="#" className={`flex font-medium mt-2 ${selectedItem === 'username' ? 'text-blue-600' : ''}`} onClick={() => HandleClick('username')}>Username
+                            <img src={DropArrow} alt="" className=' h-2 mt-2.5 ml-3' />
                         </a>
                 </nav>
                 <div className=' h-6 mt-7'>
                     <a href="https://"><img src={group} alt="" /></a>
                 </div>
             </div>
-            <div className=' mt-12'>
-                <img src={unsplash} className=' absolute' alt="" />
-                <img src={frame1} className=' relative ml-20 mt-[10px]' alt="" />
-                <img src={frame2} className=' relative ml-[70px] mt-10 ' alt="" />
+            <div className=' mt-12 '>
+                <img src={unsplash} alt="" className=' absolute' />
+                
+            </div>
+            <div className=' h-[100px] w-[200px] ml-10 relative py-5 '>
+                    <img src={frame1} alt="" className=' w-[150px] ml-8 '/>
+                    <div className='flex h-[30px] w-[150px] bg-white ml-8 justify-center mt-1'>
+                        <a href='#' className='flex'><button type='button'><p className=' font-semibold'>View Profile</p></button>
+                        <img src={ArrowLeft} alt="" className=' h-4 ml-3 mt-2' /></a>
+                    </div>
+
             </div>
         </header>
         
