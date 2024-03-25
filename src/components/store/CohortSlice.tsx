ae2364692783fdd3ab0a31dc122dd8fb5ad2dbc5
Saheed Dammy <saidex1@ymail.com>
2024-03-25 11:28:12 +0100

    update

---
+++ src/components/store/CohortSlice.tsx
@@ -0,0 +1,32 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+export interface IAuthState {
+
+    authState: CohortObject[];
+}
+
+interface CohortObject {
+    cohort: string;
+    description: string;
+    program: string;
+    startDate: string;
+    endDate: string;
+    cohortAvatar: string
+}
+const initialState: IAuthState = {
+    authState: [],
+};
+
+export const cohortSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        addCohort: (state, action: PayloadAction<any>) => {
+            state.authState.push(action.payload);
+            console.log(action.payload);
+        }
+    },
+});
+
+export const addCohort = cohortSlice.actions;
+export const authReducer = cohortSlice.reducer;
